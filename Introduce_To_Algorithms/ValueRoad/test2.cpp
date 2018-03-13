ee0a27a31a94d4d7ad8c45a979c736a49d86b0d9
zhaoqian <qianzhaoapollo@163.com>
2018-03-13 15:06:55 +0800

    middle

--- Introduce_To_Algorithms/ValueRoad/test2.cpp
+++ Introduce_To_Algorithms/ValueRoad/test2.cpp
@@ -4,14 +4,15 @@
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <list>
 using namespace std;
 
 struct csvdata {
     string date;
     string road_id;
     string long_late;
-    string longtitude;
-    string latetitude;
+    string start_point;
+    string end_point;
     int len;
     int total_flow;
     int trunk_flow;
@@ -25,6 +26,7 @@ struct cmp {
 };
 
 priority_queue<csvdata, vector<csvdata>, cmp> Q;
+priority_queue<csvdata, vector<csvdata>, cmp> R;
 
 int main()
 {
@@ -35,7 +37,7 @@ int main()
     string value;
     ifstream infile(filename.c_str());
     ofstream out;
-    while(infile.good()) {
+    while(!infile.eof()) {
         getline(infile, value);
 
         string tmp_total;
@@ -61,10 +63,11 @@ int main()
             if(count == 1)
                 intp.road_id += value[i];
             if(count == 2) {
+                intp.long_late += value[i];
                 if(count_pos == 0)
-                    intp.longtitude += value[i];
+                    intp.start_point += value[i];
                 if(count_pos == 1)
-                    intp.latetitude += value[i];
+                    intp.end_point += value[i];
             }
 
             if(count == 3)
@@ -80,48 +83,111 @@ int main()
 
             i++;
         }
+        if(intp.long_late.empty())
+            continue;
 
         Q.push(intp);
         //cout<<intp.date<<" "<< intp.road_id <<" "<< intp.long_late <<" "<< intp.total_flow <<" "<< intp.trunk_flow <<" "<< intp.car_flow <<endl;
 
     }
     cout<<"Q.size(): "<<Q.size()<<endl;
+    int length = Q.size();
+    R = Q;cout<<R.size()<<endl;
+
     int num=0;
+    int countnull=0;
 
+    list<csvdata> full_road;
+    list<csvdata> swap_place;
+
+    int sameflowcount=0;
+    int sametotalflow=0;
+    int sametrunkflow=0;
+    int diffflag = 0;
+    queue<int> total_flow;
+    queue<int> total_flow_copy;
     while(!Q.empty()) {
-        cout<<Q.top().longtitude<<" "<<Q.top().latetitude<<endl;
+        //cout<<Q.top().start_point<<" "<<Q.top().end_point<<" "<<Q.top().total_flow<<endl;
+        //cout<<Q.top().long_late<<" "<<Q.top().total_flow<<endl;
+        if(Q.top().long_late.empty())
+            countnull++;
 
-        Q.pop();
         num++;
+
+        if(sametotalflow != Q.top().total_flow) {
+            diffflag = 1;
+            sametotalflow = Q.top().total_flow;//cout<<sametotalflow;
+            sametrunkflow = Q.top().trunk_flow;
+            if(sameflowcount != 0 )
+                total_flow.push(sameflowcount);
+            sameflowcount = 0;
+        }
+        if(num == length && diffflag == 0) {
+            total_flow.push(++sameflowcount);
+        }
+        if(num == length && diffflag == 1) {
+            total_flow.push(1);
+        }
+        else {
+            diffflag = 0;
+            sameflowcount++;
+        }
+        Q.pop();
+
     }
 
-    cout<<"num: "<<num;
+    //total_flow_copy = total_flow;
+    //cout<<total_flow.size()<<endl;
+    int sum=0;
+    cout<<"+++++++++++++++++++++"<<endl;
+    while(!total_flow.empty()) {
+        sum+=total_flow.front();
+        //cout<<total_flow.front()<<endl;
+        total_flow_copy.push(total_flow.front());
+        total_flow.pop();
+    }
+
+    while(!R.empty()) {
+        int index=0;
+        while(index < total_flow_copy.front()) {
+            swap_place.push_back(R.top());
+            index++;//cout<<total_flow_copy.front()<<endl;
+            R.pop();
+        }
+        total_flow_copy.pop();
+
+        full_road.push_back(swap_place.front());
+
+        //for(list<csvdata>::iterator it=swap_place.begin(); it!=swap_place.end(); it++) {
+            //csvdata tem = *it;
+            //full_road.push_back(tem);
+       for(unsigned int k=0; k<swap_place.size(); k++) {
+            for(list<csvdata>::iterator ik=swap_place.begin(); ik!=swap_place.end(); ik++) {
+                csvdata kem = *ik;
+                if(full_road.back().end_point.compare(kem.start_point) == 0) {
+                    full_road.push_back(kem);//cout<<full_road.back().end_point<<endl;
+                }
+                if(full_road.front().start_point.compare(kem.end_point) == 0) {
+                    full_road.push_front(kem);//cout<<full_road.front().start_point<<endl;
+                }
+            }
+        }
+
+        for(list<csvdata>::iterator it=full_road.begin(); it!=full_road.end(); it++) {
+            csvdata tem = *it;
+            cout<<tem.start_point<<" "<<tem.end_point<<endl;
+        }
+
+
+
+            //cout<<tem.start_point<<" "<<tem.end_point<<endl;
+
+        //}
+
+    }
+
+    cout<<"num: "<<num<<"; sum: "<<sum<<"; countnull: "<<countnull;
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
